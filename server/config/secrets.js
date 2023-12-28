c343ac2b27380571996cd8c3bff9cd0a7a87ea43
ftrichardson <flynnrichardson21@gmail.com>
2023-12-27 18:59:56 -0800

    Update database

--- server/config/secrets.js
+++ server/config/secrets.js
@@ -1,4 +1,4 @@
 module.exports = {
     token : "secret-starter-mern",
-    mongo_connection: "mongodb+srv://flynnrichardson21:wW6qy4eIYqJjafA6@habit-coach.ea5wz1a.mongodb.net/habit-coach?retryWrites=true&w=majority",
+    mongo_connection: "mongodb+srv://flynnrichardson21:ILJ4UjQV63GAX4An@cluster0.0o3gmrz.mongodb.net/",
 };
